f831ea01dd195128d70c739675af5c08c83dc4d1
narmusrm-dev <narmusrm@gmail.com>
2026-07-15 15:17:09 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
+export default function Home() {
+  return (
+   <>
+   <Hero />
+   <About />
+   <Skills />
+   <Contact />
+    <Footer />  
+   </>
+  );
+}
